e506118895b69caf46acbf6d4de5a758eba84701
Ezequiel Telias <ezetelias95@gmail.com>
2019-10-14 13:12:07 -0300

    removed unused dependencies and add breadcrumb separator const

--- view/src/components/Breadcrumb/Breadcrumb.jsx
+++ view/src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import "../../styles/breadcrumb/breadcrumb.css";
-
+const breadcrumbSeparator = ">"
 // Returns the breadcrumb separated with an "/" except for the last item
 function Breadcrumb({ productCategoriesArray }){
      return <ul className={"breadcrumb-bar"}>
              {productCategoriesArray.map((category, index) => (
                  <li>
                      <Link key={category} to={`/items?search=${category}`}> {category} </Link>
-                     { index !== productCategoriesArray.length-1 ? "/" : null}
+                     { index !== productCategoriesArray.length-1 ? breadcrumbSeparator : null}
                  </li>
                  ))}
             </ul>
